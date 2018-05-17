88cb0d414ee050e1368e3eb3f35276ba1a39238f
James Olds <james@oldsmail.com>
2018-05-17 15:59:11 -0500

    Bash clean up

--- lib/getver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/vbash
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
-source /opt/vyatta/etc/functions/script-template
-source /etc/bash_completion.d/vyatta-cfg
-source /etc/bash_completion.d/vyatta-op
-
-show system image | grep "default boot"
