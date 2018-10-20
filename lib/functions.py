f04319b1fbba17b8897c103381878b92438c9d78
James Olds <jamesolds@olds-mbp.oldsmail.com>
2018-10-19 20:51:42 -0500

    Merge branch oldsj/ansible into master

--- lib/functions.py
+++
@@ -1,8 +0,0 @@
-from paramiko import SSHClient, AutoAddPolicy
-
-def createSSHClient(server):
-    client = SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect(server)
-    return client
--- lib/letsencrypt/prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/vbash
-sudo mv /tmp/acme.sh /config/.acme.sh/acme.sh
-sudo mv /tmp/renew.acme.sh /config/scripts/renew.acme.sh
-sudo chmod 755 /config/.acme.sh/acme.sh /config/scripts/renew.acme.sh
-sudo chown -R root:root /config/.acme.sh /config/scripts/renew.acme.sh
-sudo chmod -R 755 /config/.acme.sh /config/scripts/renew.acme.sh
