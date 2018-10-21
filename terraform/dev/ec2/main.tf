90b954741061cb5fe0f7eb66dd2735f5b8ed1c87
James Olds <jamesolds@olds-mbp.oldsmail.com>
2018-10-21 08:09:17 -0500

    Add instane security group

--- terraform/dev/ec2/main.tf
+++ terraform/dev/ec2/main.tf
@@ -2,4 +2,18 @@ resource "aws_instance" "test_instance" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   subnet_id     = "${element(data.aws_subnet_ids.private.ids, count.index)}"
+  security_groups = ["${aws_security_group.allow_cgw_lan.id}"]
+}
+
+resource "aws_security_group" "allow_cgw_lan" {
+  name        = "allow_cgw_lan"
+  description = "Allow inbound traffic from the customer gateway"
+  vpc_id      = "${data.aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.7.0/24"]
+  }
 }
